ac8fb9dcdbc48969b64c91cf40c5e31531e9fae6
Chandler <chandlersamkeep@gmail.com>
2021-02-09 19:42:33 +0000

    v1.1.1 Updated RPi SPI
    -Changelog
    -Updated SPI communication on Pi- rewritten receiving response on MISO pin. Read on return of xfer3.

--- Pi/SPI.py
+++ Pi/SPI.py
@@ -1,4 +1,5 @@
-#Raspberry Pi SPI testing, Chandler Keep, 06/02/2021
+#Raspberry Pi SPI Main, Chandler Keep, 06/02/2021
+from time import sleep;
 import spidev;
 #SPI bus 0 pins 
 sMosi = 19;
@@ -7,98 +8,24 @@ sClck = 23;
 sCs = 24;
 #Using SPI0
 spi_bus = 0;
-#SPI secondary
+#SPI secondary CE
 spi_device = 0;
 #SPI speed
 sMhz = 4000000;
+global spi;
 
 def sendData():
-    data = 0x80;
-    receive = spi.transfer(data);
-    #Checks response
-    receive = spi.transfer(data);
-    rdata = receive[0];
-    if(rdata != 0x80):
-        print("rip");
-        quit();
-    else:
-        print("Yay");
-        quit();
+    data = [10, 20, 30, 40, 50, 60, 70, 80];
+    received = spi.xfer3(data);
+    print("received bytes " + str(received));
 
 spi = spidev.SpiDev();
+#Disable chip select - only 1 secondary
+spi.no_cs;
 spi.open(spi_bus, 0);
 #Set speed to 4Mhz (same as microbit comm)
 spi.max_speed_hz = sMhz;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while(True):
+    print("Sending data");
+    sendData();
     
-    loop();
