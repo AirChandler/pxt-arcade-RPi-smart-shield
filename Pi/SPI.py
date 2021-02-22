c95d0dac312be59afcb92dcda73f13fc97fb41df
Chandler <chandlersamkeep@gmail.com>
2021-02-22 00:40:43 +0000

    v1.2 Updated RPi SPI
    Changelog-
    -Updated Raspberry pi SPI to reliably read STM32F0 spi data

--- Pi/SPI.py
+++ Pi/SPI.py
@@ -10,22 +10,36 @@ sCs = 24;
 spi_bus = 0;
 #SPI secondary CE
 spi_device = 0;
-#SPI speed
-sMhz = 4000000;
+#SPI speed on STM32f0 Bus
+sMhz = 2400000;
 global spi;
 
+def response():
+    #give time for response
+    sleep(0.001);
+    res = spi.readbytes(1);
+    #Cycle buffer
+    sleep(0.001);
+    return res;
+
 def sendData():
-    data = [10, 20, 30, 40, 50, 60, 70, 80];
-    received = spi.xfer3(data);
-    print("received bytes " + str(received));
+    data = [2];
+    #Transfer bytes
+    spi.xfer(data);
+    #Retrieve response
+    resp = response();
+    print("received bytes " + str(resp));
 
 spi = spidev.SpiDev();
 #Disable chip select - only 1 secondary
-spi.no_cs;
+#spi.no_cs;
 spi.open(spi_bus, 0);
 #Set speed to 4Mhz (same as microbit comm)
 spi.max_speed_hz = sMhz;
 while(True):
-    print("Sending data");
     sendData();
+    #Enables 60 updates per second;
+    sleep(1/60);
+
+
     
